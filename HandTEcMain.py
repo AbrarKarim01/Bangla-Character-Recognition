2e2d4fa6e9f5ff87d869d07eca8e954af27ef5eb
arafa <abrar.karim@northsouth.edu>
2022-12-03 20:33:43 +0600

    Version1_Test - Not Running Indentation Problem

---
+++ HandTEcMain.py
@@ -0,0 +1,48 @@
+# import by installing packages:
+import cv2
+import mediapipe as mp
+import time
+
+# Running a webCam
+
+
+# Objects
+
+cap = cv2.VideoCapture(0)  # opening a default camera
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()  # creating object with library default parameter
+mpDraw = mp.solutions.drawing_utils  # drawing landmarks of 21 points with methods
+
+# FrameRate
+
+pTime = 0
+Ctime = 0
+
+while True:
+
+    success, img = cap.read()  # capturing the frame rate
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # convert to RGB ( hand class only support RGB )
+    results = hands.process(imgRGB)  # process the frame by receiving the object
+
+    # Checking multiple hands
+
+    if results.multi_hand_landmarks:
+            # extracting multiple hands
+        for handLandmarks in results.multi_hand_landmarks:
+            # checking index by finding id and LandMarks information by Using X, Y, Z Coordinates
+        for id, LandMark in enumerate(handLandmarks.landmark):
+            h, w, c= img.shape
+            cx, cy = int(LandMark.x * w), int(LandMark.y * h)
+            print(img, LandMark)
+
+
+        mpDraw.draw_landmarks(img, handLandmarks, mpHands.HAND_CONNECTIONS)  # drawing the landmarks with lines
+
+    # FrameRate
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (250, 0, 255), 3)  # showing frameRate
+    cv2.imshow("Image", img)  # capturing the image as video
+    cv2.waitKey(1)  # capturing the frame rate
